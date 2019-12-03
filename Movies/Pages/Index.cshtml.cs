ea2521014d80f6192570b96b61ebc347c038cf93
Zane Roberts <zmroberts17@ksu.edu>
2019-12-03 14:54:49 -0600

    This should complete class work

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<Movie> Movies;
+        public IEnumerable<Movie> Movies;
 
         [BindProperty]
         public string search { get; set; }
@@ -23,11 +23,14 @@ namespace Movies.Pages
         [BindProperty]
         public float? maxIMDB { get; set; }
 
+        [BindProperty]
+        public string sort { get; set; }
+
 
 
         public void OnGet()
         {
-            Movies = MovieDatabase.All;
+            Movies = MovieDatabase.All.OrderBy(movie => movie.Title);
         }
 
         public void OnPost()
@@ -36,20 +39,53 @@ namespace Movies.Pages
 
             if (search != null)
             {
-                Movies = MovieDatabase.Search(Movies, search);
+                Movies = Movies.Where(movie => movie.Title.Contains(search, StringComparison.OrdinalIgnoreCase)
+                || movie.Director != null && movie.Director.Contains(search, StringComparison.OrdinalIgnoreCase));
+                //Movies = MovieDatabase.Search(Movies, search);
             }
 
             if(mpaa.Count != 0)
             {
-                Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
+                Movies = Movies.Where(movie => mpaa.Contains(movie.MPAA_Rating));
+                //Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
             }
 
             if(minIMDB != null)
             {
-                Movies = MovieDatabase.FilterByMinIMDB(Movies, (float)minIMDB);
+                Movies = Movies.Where(movie => movie.IMDB_Rating != null && movie.IMDB_Rating >= minIMDB);
+                //Movies = MovieDatabase.FilterByMinIMDB(Movies, (float)minIMDB);
             }
 
-            
+            if (maxIMDB != null)
+            {
+                Movies = Movies
+                    .Where(movie => movie.IMDB_Rating != null)
+                    .Where(movie => movie.IMDB_Rating <= maxIMDB);
+            }
+
+            if (sort != null)
+            {
+                Movies = Movies.OrderBy<Movie, string>(movie =>
+                {
+                    switch (sort)
+                    {
+                        case "title":
+                            return movie.Title;
+                        case "director":
+                            if (movie.Director == null) return "";
+                            string[] parts = movie.Director.Split(" ");
+                            return parts[parts.Length -1];
+                        case "year":
+                            return movie.Release_Year;
+                        case "imdb":
+                            return movie.IMDB_Rating.ToString();
+                        case "rt":
+                            return movie.Rotten_Tomatoes_Rating.ToString();
+                        default:
+                            return movie.Title;
+                    }
+                });
+            }
         }
     }
 }
